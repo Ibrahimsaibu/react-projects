35b4b7f500e5442533f11ab7bf9a37ce9830df9c
Ibrahim Saibu <ibrahimsaibu2010@gmail.com>
2022-01-13 04:36:57 -0800

    birthday reminder

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [familyMembers, setFamilyMembers] = useState(data);
+  const removeFamilyMember = (id) => {
+    let remainingMembers = familyMembers.filter(
+      (familyMember) => familyMember.id !== id
+    );
+    setFamilyMembers(remainingMembers);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{familyMembers.length} Birthdays today</h3>
+        <List
+          familyMembers={familyMembers}
+          removeFamilyMember={removeFamilyMember}
+        />
+        <button onClick={() => setFamilyMembers([])}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ familyMembers, removeFamilyMember }) => {
   return (
     <>
-      <h2>list component</h2>
+      {familyMembers.map((familyMember) => {
+        const { id, name, age, image } = familyMember;
+        return (
+          <article key={id} className=" personContainer">
+            <div className="person ">
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years old </p>
+              </div>
+            </div>
+            <div className="x">
+              <button onClick={() => removeFamilyMember(id)}>x</button>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
